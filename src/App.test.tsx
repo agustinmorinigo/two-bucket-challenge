5d8571d8aa78b7fd698c32b46336cc1ed29d939c
Agustin Morinigo <agustinmorinigo1999@gmail.com>
2025-05-03 16:50:42 -0300

    Test: Test Biome

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,10 +2,10 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders a heading', () => {
-  render(<App />);
-  expect(screen.getByRole('heading')).toBeInTheDocument();
+    render(<App />);
+    expect(screen.getByRole('heading')).toBeInTheDocument();
 });
 
 test('failing test', () => {
-  expect(1 + 1).toBe(2);
+    expect(1 + 1).toBe(2);
 });
