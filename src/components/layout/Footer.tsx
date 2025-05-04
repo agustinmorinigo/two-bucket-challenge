04f9d14e1b77186b04c85d00810e4ea20b6cfad4
Agustin Morinigo <agustinmorinigo1999@gmail.com>
2025-05-03 22:14:39 -0300

    Feature: Add the basic structure for the challenge

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="w-full bg-gray-200 p-4 text-center">
+            <p>Developed by Agustin Morinigo</p>
+        </footer>
+    );
+}
---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className="w-full bg-gray-200 p-4 text-center flex items-start">
+            <h1 className="font-bold text-xl">Two bucket challenge</h1>
+        </header>
+    );
+}
